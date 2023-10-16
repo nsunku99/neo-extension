52080dfb326685e69d5da72fc6d32feb49472401
Nitesh Sunku <nsunku99@gmail.com>
2023-10-15 17:11:44 -0700

    created react app via vite for webview

---
+++ media/main.js
@@ -0,0 +1,12 @@
+
+
+(function () {
+
+  const vscode = acquireVsCodeApi();
+
+  const counter = document.querySelector('button');
+  // counter.innerHTML = 'new text';
+  // console.log(counter);
+  console.log('hello from main js file');
+
+})();
