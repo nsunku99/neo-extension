1f516ec54cf2bc7322886b7e39aa1e9531c6979d
Nitesh Sunku <nsunku99@gmail.com>
2023-10-17 22:00:54 -0700

    making input field to receive localhost link

--- neo-webview/src/Components/Test.tsx
+++ neo-webview/src/Components/Test.tsx
@@ -1,13 +1,43 @@
 // import { useContext } from "react"
 // import { VsCodeApiContext } from "../Contexts/vsCodeContext";
+import { useEffect, useState } from "react"
 import { vscode } from "../App"
 
 export function Test({ vsConnect }: { vsConnect: vscode }) {
 
-  // const useVsCodeApi = useContext(VsCodeApiContext);
+  const [notReceived, setReceived] = useState(true);
+  const [link, setLink] = useState('');
+
+  function onClick() {
+    if (link !== '' && link.includes('http://localhost:')) {
+      vsConnect.postMessage({ command: 'live server link', text: link })
+      setLink('');
+      setReceived(false);
+    }
+  }
+
+  useEffect(() => {
+
+    window.addEventListener('message', (e) => {
+      console.log('e: ', e);
+
+    })
+
+  })
 
   return (
     <>
+      {notReceived ?
+        <div className="mx-auto flex flex-col w-[30vw] justify-around items-center my-4">
+          <input
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+            type="text"
+            className="my-2" />
+          <button onClick={onClick}>Submit LocalHost Link</button>
+        </div> :
+        null
+      }
       <div className="flex justify-around">
         <button
           className="m-4"
