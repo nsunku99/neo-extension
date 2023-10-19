8f96af96d800819468723069a27af03393f70fd1
Nitesh Sunku <nsunku99@gmail.com>
2023-10-18 19:22:20 -0700

    changed from chart js to recharts for successful donuts

--- neo-webview/src/Components/donut.tsx
+++ neo-webview/src/Components/donut.tsx
@@ -1,10 +1,9 @@
-/* Styling component for donut charts */
-// TRY A USEREF?
+// // /* Styling component for donut charts */
+// // // TRY A USEREF?
 
-import { useState, useEffect } from 'react';
-import Chart from 'chart.js/auto';
+import { ResponsiveContainer, PieChart, Pie, Cell, Label } from 'recharts';
 
-type donutProps = {
+type DonutProps = {
   donutData: number[];
   donutName: string;
   csize: number;
@@ -12,83 +11,41 @@ type donutProps = {
   color: string;
 };
 
-export default function Donut({ donutData, donutName, csize, overallScore, color }: donutProps) {
-  const [myChartState, setMyChartState] = useState<Chart | null>(null);
+const DonutChart = ({ donutData, donutName, csize, overallScore, color }: DonutProps) => {
 
-  useEffect(() => {
-
-    const chartRefZero = document.getElementById(`0`) as HTMLCanvasElement;
-    if (chartRefZero) {
-      if (myChartState) {
-        myChartState.destroy();
-      }
-    }
-
-    const chartRef = document.getElementById(`pie-chart-${donutName}`) as HTMLCanvasElement;
-
-    if (chartRef) {
-      if (myChartState) {
-        myChartState.destroy();
-      }
-
-      const donutLabel = {
-        id: 'doughnutLabel',
-        beforeDatasetsDraw(chart: Chart) {
-          const { ctx } = chart;
-          ctx.save();
-          const xCoor = chart.getDatasetMeta(0).data[0].x
-          const yCoor = chart.getDatasetMeta(0).data[0].y
-          // plug in score number here
-          ctx.fillText((overallScore).toString(), xCoor, yCoor)
-          ctx.textAlign = 'center'
-          ctx.font = '20px sans-serif'
-        }
-      }
-
-      const myChart = new Chart(chartRef, {
-        type: 'doughnut',
-        data: {
-          datasets: [
-            {
-              data: [70, 30],
-              backgroundColor: [color, 'white'],
-            },
-          ],
-        },
-        options: {
-          plugins: {
-            title: {
-              display: true,
-              text: donutName,
-              font: { size: donutName === 'Overall' ? 24 : 16 },
-            },
-          },
-        },
-        plugins: [donutLabel],
-      });
-
-      const datasets = myChart.data.datasets;
-      datasets[0].data = donutData;
-
-      if (myChart) {
-        myChart.update();
-        setMyChartState(myChart as Chart);
-      }
-    }
-
-
-  }, [donutData]);
+  console.log('donut data: ', donutData);
 
   return (
-    <div
-      style={
-        {
-          height: `${csize}px`,
-          width: `${csize}px`
-        }}>
-      <canvas
-        id={`pie-chart-${donutName}`}
-      ></canvas>
-    </div>
+    <ResponsiveContainer width={csize} height={csize}>
+      <PieChart>
+        <Pie
+          data={[
+            { name: 'Segment 1', value: donutData[0] },
+            { name: 'Segment 2', value: donutData[1] },
+          ]}
+          cx="50%"
+          cy="50%"
+          innerRadius="40%" // Adjust the inner radius for the donut effect
+          outerRadius="80%"
+          fill="#8884d8"
+          dataKey='value'
+        >
+          <Cell key="cell-1" fill={color} />
+          <Cell key="cell-2" fill="white" />
+          <Label
+            value={overallScore.toFixed(2)}
+            position="center"
+            fontSize={donutName === 'Overall' ? 24 : 16}
+          />
+        </Pie>
+        <Label
+          value={donutName}
+          position="top"
+          fontSize={donutName === 'Overall' ? 24 : 16}
+        />
+      </PieChart>
+    </ResponsiveContainer>
   );
-}
+};
+
+export default DonutChart;
