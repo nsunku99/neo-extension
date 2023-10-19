92a68bbd251393e05bc1f26fe0e8d22f9b78445f
Nitesh Sunku <nsunku99@gmail.com>
2023-10-18 19:23:14 -0700

    established proper initial vals and got rid of conditional object keys

--- neo-webview/src/Utilities/useMetrics.tsx
+++ neo-webview/src/Utilities/useMetrics.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 export type metrics = {
-  name?: string,
-  data?: number[],
-  score?: number,
-  color?: string,
-  number?: number
+  name: string,
+  data: number[],
+  score: number,
+  color: string,
+  number: number
 };
 
 export type SetMetrics = React.Dispatch<React.SetStateAction<metrics>>;
 
 export function useMetrics(initialState: metrics = {
   name: '',
-  data: [],
+  data: [0, 100],
   score: 0,
   color: 'white',
   number: 0
