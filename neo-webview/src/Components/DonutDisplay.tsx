20601b897ff7bf2f12e07a794b8bfeb791e15579
Nitesh Sunku <nsunku99@gmail.com>
2023-10-21 00:29:17 -0700

    added page name to display

--- neo-webview/src/Components/DonutDisplay.tsx
+++ neo-webview/src/Components/DonutDisplay.tsx
@@ -2,12 +2,14 @@
 
 import { useState, useEffect } from 'react';
 import Donut from './Donut';
-import { metrics } from '../Utilities/useMetrics';
+import type { metrics } from '../Utilities/useMetrics';
+import type { pageObj } from '../Containers/Body';
 
-export function DonutDisplay({ metrics }:
-  { metrics: { [key: string]: metrics } }) {
+export function DonutDisplay({ metrics, pageObj }:
+  { metrics: { [key: string]: metrics }, pageObj: pageObj }) {
 
   const { overall } = metrics;
+  const { pageName } = pageObj
 
   const [chartVision, setChartVision] = useState(false);
   const [donutArr, setDonutArr] = useState([]);
@@ -29,16 +31,16 @@ export function DonutDisplay({ metrics }:
 
           if (score) {
             donuts.push(
-              <div className='flex flex-col gap-2 justify-center items-center'>
+              <div className='flex flex-col gap-2 justify-center items-center p-3'>
                 <Donut
                   donutData={data}
                   donutName={name}
                   csize={150}
-                  overallScore={score}
+                  overallScore={+score.toFixed(2)}
                   color={color}
                 />
-                <div>
-                  {`${name}: ${number} ms`}
+                <div className='text-white'>
+                  {`${name}: ${number.toFixed(2)} ms`}
                 </div>
               </div>
             )
@@ -61,12 +63,14 @@ export function DonutDisplay({ metrics }:
           id="app-main"
           className="flex flex-col justify-center items-center text-black mx-8 my-5 grow"
         >
-
           {chartVision ? <div id="all-charts">
             <div
               id="overall-donut"
-              className="flex justify-center items-center"
+              className="flex flex-col justify-center items-center"
             >
+              <h1 className='text-white text-2xl'>
+                Page <span>"{pageName[0].toUpperCase()}{pageName.slice(1).toLowerCase()}"</span> Overall Score
+              </h1>
               <Donut
                 donutData={overall.data}
                 donutName={overall.name}
